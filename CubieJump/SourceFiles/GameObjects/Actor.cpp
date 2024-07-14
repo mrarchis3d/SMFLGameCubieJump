888ccebd048e421870e33509d2dca03896c98701
mrarchis3d <49571076+mrarchis3d@users.noreply.github.com>
2024-07-13 23:08:07 -0500

    feat(creating first animations)

---
+++ CubieJump/SourceFiles/GameObjects/Actor.cpp
@@ -0,0 +1,26 @@
+﻿#include "Actor.h"
+
+void Actor::InitializeComponents()
+{
+    
+}
+
+Animation* Actor::GetAnimation() const
+{
+    return animation;
+}
+
+Actor::Actor()
+{
+    InitializeComponents();
+}
+
+Actor::~Actor()
+{
+}
+
+void Actor::Update()
+{
+
+}
+
---
+++ CubieJump/SourceFiles/GameObjects/Animation.cpp
@@ -0,0 +1,38 @@
+﻿#include "Animation.h"
+
+#include <stdexcept>
+
+Animation::Animation(
+        sf::Texture* texture,
+        AnimationType animationType,
+        sf::Vector2u imageCount,
+        float switchTime) : animationType(animationType), imageCount(imageCount),
+                            totalTime(0.f),
+                            switchTime(switchTime)
+{
+    if(texture == nullptr)
+        throw std::invalid_argument("Animation should have texture");
+    uvRect.width = texture->getSize().x / imageCount.x;
+    uvRect.height = texture->getSize().y / imageCount.y;
+}
+
+Animation::~Animation()
+{
+}
+
+void Animation::Update(int row, float deltaTime)
+{
+    currentImage.y = row;
+    totalTime += deltaTime;
+    if(totalTime>=switchTime)
+    {
+        totalTime-=switchTime;
+        currentImage.x++;
+        if(currentImage.x >= imageCount.x)
+        {
+            currentImage.x = 0;
+        }
+    }
+    uvRect.left = currentImage.x * uvRect.width;
+    uvRect.top = currentImage.y * uvRect.height;
+}
